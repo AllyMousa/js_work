fbb0563df49f436a0704f0173de3469a8ec01809
Ally Moussa <allymoussa12@gmail.com>
2024-10-31 17:18:00 +0200

    Add files via upload
    js

---
+++ js/js.js
@@ -0,0 +1,144 @@
+// let day = 23;
+
+// array
+
+// let array = [1,2,3,4,5,6,7,8,9]
+
+// let sum = 0
+// for (let i = 0; i < array.length; i++) {
+//     sum = sum + array [i]; 
+// }
+// console.log(sum)
+
+
+
+
+
+// let array = [5,6,7,8,9,98];
+// let smallest = array[0];
+// let greatest = array[0];
+// for (let i = 1; i < array.length; i++) {
+//     if (array[i] < smallest) {
+//         smallest = array[i];
+//     }
+//     if (array[i] > greatest) {
+//         greatest = array[i];
+//     }
+// }
+// console.log(`Smallest number: ${smallest}, Greatest number: ${greatest}`);
+
+
+
+
+//   work
+let arr_3 = [8,2,9]
+let arr_4 = [9,3,1]
+
+
+let result = [arr_3[0] + arr_4[0], arr_3[1] + arr_4[1], arr_3[2] + arr_4[2]]
+console.log(result)
+
+
+
+// let arr_3 = [4,6,7]
+// let arr_4 = [8,1,9]
+// sample
+//[12,7,16]
+
+
+
+
+// console.log(array['5'])
+
+// for (let i = 0; i < array.length; i++) {
+//     // console.log(array[i])
+//     if (array[i] % 2 == 0) {
+//         console.log([i])
+//     }
+// }
+
+
+
+
+// if (day == "23") {
+//     alert("this is a day")
+// }else{
+//     console.log("this is a day")
+// }
+
+// if (day == "203") {
+//     console.log("this is a day")
+// }else if (day === "23") {
+//     console.log("this is equal")
+// }else if (day == "23") {
+//     console.log("this is a dayyy")
+// }else{
+//     console.log("this is not day")
+// }
+
+
+
+
+// valiables
+
+//###################### 3 valiables ###############
+//? var
+//let
+//const
+// var text = "good afternoon"
+// console.log("text")
+
+
+
+
+
+// a = 12;
+// b = 32;
+// c = a+b;
+// console.log(c) 
+
+
+
+
+// A=pir2
+// arrow function
+// let sum = 0
+// const pi = (pi) => {
+//     return pi = 3.14
+// }
+
+// //normar function functi
+// function area_of_cicle() {
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alart message
+
+//alert("good afternoon")
+
+
+
+// consologing text
+//console.log("good afternoon")
